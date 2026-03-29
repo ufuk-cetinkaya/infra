5bb78034b1d0cf3201ac8d9f99a1725e7f5f0a98
Ufuk Cetinkaya <ufuk.cetinkya@gmail.com>
2026-03-29 11:31:47 +0300

    tls cert has added

--- argocd/bootstrap.sh
+++ argocd/bootstrap.sh
@@ -28,4 +28,12 @@ helm repo update
 helm install ingress-nginx ingress-nginx/ingress-nginx --namespace ingress-nginx --create-namespace
 kubectl create deployment nginx --image=nginx -n ingress-nginx
 
+openssl req -x509 -nodes -days 365 \
+-newkey rsa:2048 \
+-keyout tls.key \
+-out tls.crt \
+-subj "/CN=*.ufukcetinkaya.net/O=dev"
+
+kubectl create secret tls api-tls --cert=tls.crt --key=tls.key -n edonusum
+
 echo "Bootstrap completed."
